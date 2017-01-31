816dee2f754f414185effe4c5925cbbdcecea965
Vignesh S <vignesh6993@live.in>
2017-01-31 15:02:12 +0530

    Added a Read me file

---
+++ README.md
@@ -0,0 +1,6 @@
+#MyU Botkit LUIS Bot 
+
+##<i>To-Do</i>
+
+###Use Express server: <i>pending</i>
+###Use Socket.io to server to static web page: <i>pending</i>
--- my_u_bot.js
+++ my_u_bot.js
@@ -15,6 +15,13 @@ var bot = controller.spawn({
     appPassword: process.env.APP_PASSWORD
 });
 
+var Utterances = {
+    yes: new RegExp(/^(yes|yea|yup|yep|ya|sure|ok|y|yeah|yah|sounds good)/i),
+    no: new RegExp(/^(no|nah|nope|n|never|not a chance)/i),
+    quit: new RegExp(/^(quit|cancel|end|stop|nevermind|never mind)/i),
+    greetings: new RegExp(/^(hi|hello|greetings|hi there|yo|was up|whats up)/)
+};
+
 // if you are already using Express, you can use your own server instance...
 // see "Use BotKit with an Express web server"
 controller.setupWebserver(process.env.PORT, function (err, webserver) {
@@ -23,22 +30,15 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
     });
 });
 
-controller.hears(['hi'], 'message_received', luis.middleware.hereIntent, function (bot, message) {
-    
-    controller.middleware.send.use(function (bot, message, next) {
-        // do something useful...
-        if (message.topIntent.intent == 'PolicyIssuance_PayPremium') {
-            message.text = 'Hello!!!';
-        }
-        next();
-    });
+controller.hears(Utterances.greetings, 'message_received', luis.middleware.hereIntent, function (bot, message) {
+
     bot.startConversation(message, function (err, convo) {
         if (message.topIntent.intent == 'PolicyIssuance_TrackPolicy') {
             convo.say('Track policy');
             convo.next();
         } else {
             convo.say('Did someone say cookies!?!!');
-                convo.say('Top intent is: ' + JSON.stringify(message.topIntent));
+            convo.say('Top intent is: ' + JSON.stringify(message.topIntent));
         }
     });
 });
